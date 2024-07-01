893f28f8668221394e625f104bc3d9bf063c0ecb
SCOGE <scoge@125.156.182.163.naturalwireless.com>
2024-07-01 03:44:12 -0400

    fixing

---
+++ dist/assets/hashMessage-DRZYVMnK.js
@@ -0,0 +1,2 @@
+import{aB as e,aC as s,at as i,a8 as o}from"./main-BiTp6fw0.js";import"./wScoge-BfExBSv1.js";import"./i49-DSyJwZGj.js";const c=`Ethereum Signed Message:
+`;function y(t,n){const r=typeof t=="string"?e(t):t.raw instanceof Uint8Array?t.raw:s(t.raw),a=e(`${c}${r.length}`);return i(o([a,r]),n)}export{y as hashMessage};
