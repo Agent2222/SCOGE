92b822acde9dc429bfdf1c4fc4ed9e5922d55feb
SCOGE Inc. <scoge@scoge.co>
2024-10-21 19:16:29 -0400

    update

---
+++ dist/assets/eth_getTransactionCount-IH_-78E0.js
@@ -0,0 +1,1 @@
+import{e as n,aG as r}from"./main-BbiQUIhh.js";import"./wScoge-z36Oeizf.js";async function a(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return r(e)}export{a as eth_getTransactionCount};
