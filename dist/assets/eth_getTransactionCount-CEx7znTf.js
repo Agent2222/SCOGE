afd1efc35d44c2a5e455c69af56191763bbc2d1a
SCOGE Inc. <info@scoge.co>
2024-11-18 03:46:00 -0500

    reminder feature added

--- dist/assets/eth_getTransactionCount-CEx7znTf.js
+++
@@ -1,1 +0,0 @@
-import{e as n,aG as r}from"./main-BylvssbE.js";import"./wScoge-z36Oeizf.js";async function a(t,o){const e=await t({method:"eth_getTransactionCount",params:[o.address,o.blockNumber?n(o.blockNumber):o.blockTag||"pending"]});return r(e)}export{a as eth_getTransactionCount};
