08b262df04b58d3eac5ad9c884bf52ce30458125
SCOGE Inc. <info@scoge.co>
2025-01-09 00:12:38 -0500

    ccpa bug fixes

--- dist/assets/eth_estimateGas-Cot0hsxX.js
+++
@@ -1,1 +0,0 @@
-import{J as s}from"./main-DlFvGqds.js";import"./wScoge-DGG5VK00.js";async function o(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{o as eth_estimateGas};
