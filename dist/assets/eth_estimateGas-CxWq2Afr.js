9cd2ceea9ed016f26865e314cdbabb7125d2ddb7
SCOGE Inc. <info@scoge.co>
2025-02-05 15:05:26 -0500

    tweaking speed

---
+++ dist/assets/eth_estimateGas-CxWq2Afr.js
@@ -0,0 +1,1 @@
+import{J as s}from"./main-D9Rw4yiO.js";import"./wScoge-GKPzpLMX.js";async function o(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{o as eth_estimateGas};
