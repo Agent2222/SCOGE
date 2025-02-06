877d89825e80de0c2192cde2fe0ab43b20da40bf
SCOGE Inc. <info@scoge.co>
2025-02-05 22:21:29 -0500

    speed fixes

--- dist/assets/index-BohK4jSo.js
+++ dist/assets/index-BohK4jSo.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-BhpGWvi3.js";import"./main-BKUcuIvJ.js";import"./wScoge-CcoIV1Ln.js";/**
+import{n as _t,s as ce,T as N,t as H,a as C,o as ot,R as Do,p as K,y as gt}from"./index-DdTeI3gT.js";import"./main-Ejj1pGsx.js";import"./wScoge-CcoIV1Ln.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
